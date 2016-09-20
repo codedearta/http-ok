5f155ce0ad1544a83cdb23dc8b5b29b158241268
codedearta <sepp.renfer@gmail.com>
2016-09-20 12:52:36 +0200

    class syntax

--- src/http_ok.js
+++ src/http_ok.js
@@ -1,53 +1,66 @@
 const http = require('http');
 const url = require('url');
 
-function HttpOk(agent = false) {
-	this.agent = agent;
-}
+class HttpOk {
+	constructor(agent = false) {
+    this.agent = agent;
+  }
 
-HttpOk.prototype.get = function(requestOptions, expectedStatusCode = 200) {
-	let innerRequestOptions;
-
-	if(typeof requestOptions === 'string') {
-		const urlObj = url.parse(requestOptions);
-		innerRequestOptions = {
-		  hostname: urlObj.hostname,
-		  port: urlObj.port ? urlObj.port : 80,
-		  path: urlObj.pathname,
-		  method: 'GET'
-		};
-	} else {
-		innerRequestOptions = requestOptions
+  get(requestOptions, expectedStatusCode = 200) {
+		const ro = this.parseRequestOptions(requestOptions);
+		ro.method = 'GET';
+		return this.request(ro, expectedStatusCode);
 	}
 
-	return new Promise((resolve, reject) => {
-		const request = http.request(innerRequestOptions, (response) => {
-				if(response.statusCode === expectedStatusCode) {
-					response.text = () => this.bodyText(response);
-					resolve(response);
-				} else {
-					reject(response);
-				}
-			});
-			request.on('error', error => {
-				reject(error);
-			});
-			request.end();
-	});
-}
+	post(requestOptions, expectedStatusCode = 200) {
+		const ro = this.parseRequestOptions(requestOptions);
+		ro.method = 'POST';
+		return this.request(ro, expectedStatusCode);
+	}
 
-HttpOk.prototype.bodyText = function(response) {
-	return new Promise((resolve, reject) => {
-		 response.setEncoding('utf8');
-		 let body = '';
-		 response.on('data', (chunk) => {
-			 body += chunk;
-		 });
-		 response.on('end', () => {
-			 resolve(body);
-		 });
-		 response.on('error', err => reject(err));
- });
+	parseRequestOptions(requestOptions) {
+		if(typeof requestOptions !== 'string') {
+			return requestOptions;
+		}
+
+		const urlObj = url.parse(requestOptions);
+		return {
+			hostname: urlObj.hostname,
+			port: urlObj.port ? urlObj.port : 80,
+			path: urlObj.pathname,
+		};
+	}
+
+	request(requestOptions, expectedStatusCode = 200) {
+		return new Promise((resolve, reject) => {
+			const request = http.request(requestOptions, (response) => {
+					if(response.statusCode === expectedStatusCode) {
+						response.text = () => this.bodyText(response);
+						resolve(response);
+					} else {
+						reject(response);
+					}
+				});
+				request.on('error', error => {
+					reject(error);
+				});
+				request.end();
+		});
+	}
+
+	bodyText(response) {
+		return new Promise((resolve, reject) => {
+			 response.setEncoding('utf8');
+			 let body = '';
+			 response.on('data', (chunk) => {
+				 body += chunk;
+			 });
+			 response.on('end', () => {
+				 resolve(body);
+			 });
+			 response.on('error', err => reject(err));
+	 });
+	}
 }
 
 module.exports = HttpOk;
--- test/http_ok.spec.js
+++ test/http_ok.spec.js
@@ -3,53 +3,68 @@ const http = require('http');
 const HttpOk = require('../src/http_ok.js');
 
 describe('http-ok test suite', function() {
-  let client, server, serverPort = 12345;
+    let client, server, serverPort = 12345, requestOptions, nextResult;
 
-  before('create a test server', () => {
-    server = http.createServer((req, res) => {
-      this.nextResult = 'hello';
-      //res.statusCode = 500;
-      res.end(this.nextResult);
+    before('create a test server', () => {
+        server = http.createServer((req, res) => {
+            res.statusCode = nextResult.statusCode;
+            res.end(nextResult.body);
+        });
+        server.on('clientError', (err, socket) => {
+            socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+        });
+        server.listen(serverPort);
     });
-    server.on('clientError', (err, socket) => {
-      socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+
+    beforeEach('given: initialize nextResult and requestOptions', () => {
+        nextResult = { body : 'hello', statusCode : 200 };
+        requestOptions = { hostname: 'localhost', port: serverPort, path: '' };
+        client = new HttpOk();
     });
-    server.listen(serverPort);
-  });
 
-  before('create a new HttpOk client',() => { client = new HttpOk(); });
+    beforeEach('given: create a new HttpOk client', () => {
+        client = new HttpOk();
+    });
 
-  it('get() should return a promise and resolved when statusCode is 200', function(done) {
-		client.get('http://www.nzz.ch')
-    .then(response => assert.equal(response.statusCode, 200))
-    .then(error => done(error));
-  });
+    it('get() should return a promise and resolved when statusCode is 200', function() {
+      const response = client.get(requestOptions); // when
+      return response.then(response => assert.equal(response.statusCode, 200)); // then
+    });
 
-	it('get() should return a rejected promise when hostname could not been resolved', function(done) {
-		client.get('http://www.nzz.chsdf')
-    .catch(err => assert.equal(err.code, 'ENOTFOUND'))
-    .then(error => done(error));
-	});
+    it('get() should return a rejected promise containing the response as the error when statusCode is not 200', function() {
+      nextResult.statusCode = 301; // given
+      const response = client.get(requestOptions); // when
+      return response.catch(err => assert.equal(err.statusCode, 301)); // then
+    });
 
-	it('get() should return a rejected promise containing the response as the error when statusCode is not 200', function(done) {
-		client.get('http://www.nzz.ch/erter').catch(err => {
-  			assert.equal(err.statusCode, 301);
-  			assert.equal(err.statusMessage, 'Moved Permanently');
-		}).then(error => done(error));
-	});
+    it('get() should return a resolved promise when statusCode is as expected equals 301', function() {
+      // given
+      nextResult.body = 'Moved Permanently'
+      nextResult.statusCode = 301;
 
+      const response = client.get(requestOptions, 301); // when
+      return response.then(response => assert.equal(response.statusCode, 301)); //then
+    });
 
-  it('get() should return a resolved promise when statusCode is as expected equals 301', function(done) {
-    client.get('http://www.nzz.ch/erter', 301)
-    .then(response => assert.equal(response.statusCode, 301))
-    .then(error => done(error));
-  });
+    it('get() should return a rejected promise when statusCode is not as expected equals 301', function() {
+      // given
+      nextResult.body = 'Moved Permanently'
+      nextResult.statusCode = 301;
 
-  it('get() should return a resolved promise containing an async function to read the body text', function(done) {
-    client.get({ hostname: 'localhost', port: serverPort }, 200)
-    .then(response => response.text().then(te => assert.equal(te, 'hello'))
-    .then(error => done(error)));
-  });
+      const response = client.get(requestOptions, 200); // when
+      return response.catch(err => assert.equal(err.statusCode, 301)); // then
+    });
 
-	after('close mock server', () => server.close());
+    it('get() -> response should have a function text() returning a promise to read the body text', function() {
+      const response = client.get(requestOptions, 200); // when
+      return response.then(response => response.text().then(te => assert.equal(te, 'hello'))); // then
+    });
+
+    it('get() should return a rejected promise when hostname could not been resolved', function() {
+      requestOptions.hostname = 'www.blah.chxd' // given
+      const response = client.get(requestOptions); // when
+      return response.catch(err => assert.equal(err.code, 'ENOTFOUND')); // then
+    });
+
+    after('close mock server', () => server.close());
 });
